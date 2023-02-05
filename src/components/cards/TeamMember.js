e50335ce649d939ef89fa5cd282c597f67ff5261
Netpex <Business@netpex.info>
2023-02-05 00:21:27 -0800

    [//] "OurTeam" Page progression

--- src/components/cards/TeamMember.js
+++ src/components/cards/TeamMember.js
@@ -5,23 +5,41 @@ import '../../styles/components/TeamMember.scss';
 
 import { useState } from 'react';
 
-
-function handleClick() {
-    console.log('clicked!');
-  }
-
 function includeSlash(role) {
    if (role) {
     return "/"
    }
 }
 
+
 export default function TeamMember(props) {
 
+    function showContent(e) {
+        const container = e.target.parentElement.getElementsByClassName("information-container")[0];
+        const icon = e.target.parentElement.getElementsByClassName("fa-square-caret-down")[0];
+        if (container.classList.contains("hidden")) {
+            container.classList.remove("hidden");
+            container.classList.add("grow");
+            icon.classList.add("rotate-180");
+            icon.classList.remove("rotate-0");
+        } else {
+            container.classList.remove("grow");
+            container.classList.add("hidden");
+            icon.classList.remove("rotate-180");
+            icon.classList.add("rotate-0");
+        }
+    }
+
     return (
-       <div className="teamMember-container">
-        <i class="fa-solid fa-square-caret-down"></i>
-        <h5>{props.member} - {props.time} {includeSlash(props.role)} {props.role}</h5>            
-       </div>
+        <div className="teamMember-container">
+            <i class="fa-solid fa-square-caret-down"></i>
+            <h5 onClick={showContent}>{props.member} - {props.time} {includeSlash(props.role)} {props.role}</h5>
+            <div className="break"></div>         
+            <div className="hidden information-container">
+                <img src={props.image}></img>
+                <h3>{props.timeexpanded}</h3>
+                <p>{props.story}</p>
+            </div>
+        </div>
     )
 }
--- src/pages/OurTeam.js
+++ src/pages/OurTeam.js
@@ -15,18 +15,57 @@ export default function OurTeam() {
             <h1>Djs and Staff</h1>
             <div className="announcers">
             <h2>- Announcers</h2>
-            <TeamMember member="John Doe" time="Mornings" role="News Director"/>
-            <TeamMember member="Jane Doe" time="Afternoons" role=""/>
+            <TeamMember 
+            member="John Doe"
+            time="Mornings" 
+            role="News Director"
+            timeexpanded="Weekdays 5:30-noon & Sat 6-10am"
+            image="//www.kxxo.com/wp-content/uploads/ty-flint-mornings150.jpg"
+            story="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sapien dolor, suscipit varius lacinia nec, posuere at nisi. Pellentesque vel porttitor turpis. Etiam ornare nisi vitae quam tristique lacinia. Integer luctus non urna at mattis. Proin at est vestibulum, volutpat nibh nec, ornare tortor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nullam in lobortis odio, eu ullamcorper nisi. Fusce at lacinia magna, in fringilla nisl. Pellentesque posuere, turpis vitae gravida euismod, risus orci euismod lectus, at luctus diam ex quis eros. Phasellus et porttitor est. Nullam sollicitudin neque vitae suscipit euismod."
+            />
+            <TeamMember 
+            member="Jane Doe"
+            time="Afternoons" 
+            timeexpanded="Weekdays noon-7pm"
+            image="//www.kxxo.com/wp-content/uploads/ty-flint-mornings150.jpg"
+            story="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sapien dolor, suscipit varius lacinia nec, posuere at nisi. Pellentesque vel porttitor turpis. Etiam ornare nisi vitae quam tristique lacinia. Integer luctus non urna at mattis. Proin at est vestibulum, volutpat nibh nec, ornare tortor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nullam in lobortis odio, eu ullamcorper nisi. Fusce at lacinia magna, in fringilla nisl. Pellentesque posuere, turpis vitae gravida euismod, risus orci euismod lectus, at luctus diam ex quis eros. Phasellus et porttitor est. Nullam sollicitudin neque vitae suscipit euismod."
+            />
             </div>
             <div className="marketing">
             <h2>- Marketing</h2>
-            <TeamMember member="John Doe" time="Mornings" role="News Director"/>
-            <TeamMember member="Jane Doe" time="Afternoons" role=""/>
+            <TeamMember 
+            member="John Doe"
+            time="Mornings" 
+            role="News Director"
+            timeexpanded="Weekdays 5:30-noon & Sat 6-10am"
+            image="//www.kxxo.com/wp-content/uploads/ty-flint-mornings150.jpg"
+            story="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sapien dolor, suscipit varius lacinia nec, posuere at nisi. Pellentesque vel porttitor turpis. Etiam ornare nisi vitae quam tristique lacinia. Integer luctus non urna at mattis. Proin at est vestibulum, volutpat nibh nec, ornare tortor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nullam in lobortis odio, eu ullamcorper nisi. Fusce at lacinia magna, in fringilla nisl. Pellentesque posuere, turpis vitae gravida euismod, risus orci euismod lectus, at luctus diam ex quis eros. Phasellus et porttitor est. Nullam sollicitudin neque vitae suscipit euismod."
+            />
+            <TeamMember 
+            member="Jane Doe"
+            time="Afternoons" 
+            timeexpanded="Weekdays noon-7pm"
+            image="//www.kxxo.com/wp-content/uploads/ty-flint-mornings150.jpg"
+            story="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sapien dolor, suscipit varius lacinia nec, posuere at nisi. Pellentesque vel porttitor turpis. Etiam ornare nisi vitae quam tristique lacinia. Integer luctus non urna at mattis. Proin at est vestibulum, volutpat nibh nec, ornare tortor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nullam in lobortis odio, eu ullamcorper nisi. Fusce at lacinia magna, in fringilla nisl. Pellentesque posuere, turpis vitae gravida euismod, risus orci euismod lectus, at luctus diam ex quis eros. Phasellus et porttitor est. Nullam sollicitudin neque vitae suscipit euismod."
+            />
         </div>
         <div className="support">
             <h2>- Support Team</h2>
-            <TeamMember member="John Doe" time="Mornings" role="News Director"/>
-            <TeamMember member="Jane Doe" time="Afternoons" role=""/>
+            <TeamMember 
+            member="John Doe"
+            time="Mornings" 
+            role="News Director"
+            timeexpanded="Weekdays 5:30-noon & Sat 6-10am"
+            image="//www.kxxo.com/wp-content/uploads/ty-flint-mornings150.jpg"
+            story="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sapien dolor, suscipit varius lacinia nec, posuere at nisi. Pellentesque vel porttitor turpis. Etiam ornare nisi vitae quam tristique lacinia. Integer luctus non urna at mattis. Proin at est vestibulum, volutpat nibh nec, ornare tortor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nullam in lobortis odio, eu ullamcorper nisi. Fusce at lacinia magna, in fringilla nisl. Pellentesque posuere, turpis vitae gravida euismod, risus orci euismod lectus, at luctus diam ex quis eros. Phasellus et porttitor est. Nullam sollicitudin neque vitae suscipit euismod."
+            />
+            <TeamMember 
+            member="Jane Doe"
+            time="Afternoons" 
+            timeexpanded="Weekdays noon-7pm"
+            image="//www.kxxo.com/wp-content/uploads/ty-flint-mornings150.jpg"
+            story="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sapien dolor, suscipit varius lacinia nec, posuere at nisi. Pellentesque vel porttitor turpis. Etiam ornare nisi vitae quam tristique lacinia. Integer luctus non urna at mattis. Proin at est vestibulum, volutpat nibh nec, ornare tortor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nullam in lobortis odio, eu ullamcorper nisi. Fusce at lacinia magna, in fringilla nisl. Pellentesque posuere, turpis vitae gravida euismod, risus orci euismod lectus, at luctus diam ex quis eros. Phasellus et porttitor est. Nullam sollicitudin neque vitae suscipit euismod."
+            />
         </div>
         </div>
        </div>
